49342baed326ea0b2ef6f45b66b204eba41ba06f
MazmurRonaldo7 <100758017+MazmurRonaldo7@users.noreply.github.com>
2022-04-07 10:19:21 +0700

    Add files via upload

---
+++ Tugas9_2_php_mazmur_ronaldo_SAR/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas9 PHP</title>
+    <link href="tugas9_php_mazmur_ronaldo_SAR" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/slick.css" type="text/css" /> 
+    <link rel="stylesheet" href="css/templatemo-style.css">
+
+</head>
+<body>
+    <video autoplay muted loop id="bg-video">
+        <source src="video/gfp-astro-timelapse.mp4" type="video/mp4">
+    </video>
+    <div class="page-container">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="cd-slider-nav">
+              <nav class="navbar navbar-expand-lg" id="tm-nav">
+                <a class="navbar-brand" href="#">Mazmur Ronaldo Panjaitan</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-supported-content" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbar-supported-content">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                      <li class="nav-item selected">
+                        <a class="nav-link" aria-current="page" href="#0" data-no="1">Home</a>
+                        <div class="circle"></div>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#0" data-no="2">Gallery</a>
+                        <div class="circle"></div>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#0" data-no="3">About</a>
+                        <div class="circle"></div>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#0" data-no="4">Contact</a>
+                        <div class="circle"></div>
+                      </li>
+                    </ul>
+                  </div>
+              </nav>
+            </div>
+          </div>          
+        </div>        
+      </div>      
+      <div class="container-fluid tm-content-container">
+        <ul class="cd-hero-slider mb-0 py-5">
+          <li class="px-3" data-page-no="1">
+            <div class="page-width-1 page-left">
+              <div class="d-flex position-relative tm-border-top tm-border-bottom intro-container">
+                <div class="intro-left tm-bg-dark">
+                  <h2 class="mb-4">Welcome to My Profile</h2>
+                  <p class="mb-4">
+                    Pertama-tama saya memanjatkan puji syukur kehadirat <font color="yellow" <b>Tuhan yg Maha Esa</b></font>
+                    atas segala rahmatnya yang telah dilimpahkan kepada semuanya, sehingga
+                    saya mampu menjalankan tugas sehari-hari dengan baik. </p>
+                  <p class="mb-0">
+                    Perkenalkan nama saya mazmur ronaldo panjaitan sering di panggil ronald, keseharian saya hanya melaksanakan kuliah dan mengerjakan tugas-tugas yg di berikan. Saya dari kota Pekanbaru provinsi Riau, tempat kampus saya di Stmik Amik Riau, jurusan teknik informatika </p>
+                </div>
+                <div class="intro-right">
+                  <img src="img/1.jpeg" alt="Image" class="img-fluid intro-img-1">
+                  
+                </div>
+                <div class="circle intro-circle-1"></div>
+                <div class="circle intro-circle-2"></div>
+                <div class="circle intro-circle-3"></div>
+                <div class="circle intro-circle-4"></div>
+              </div>
+              <div class="text-center">
+                <a href="#0" data-page-no="2" class="btn btn-primary tm-intro-btn tm-page-link">
+                  View Gallery
+                </a>
+              </div>            
+            </div>            
+          </li>
+          <li data-page-no="2">
+            <!-- Image Carousel -->
+            <div class="mx-auto position-relative gallery-container">
+              <div class="circle intro-circle-1"></div>
+              <div class="circle intro-circle-2"></div>
+              <div class="mx-auto tm-border-top gallery-slider">
+                <figure class="effect-julia item">
+                    <img src="img/2.jpeg" alt="Image">
+                    <figcaption>
+                        
+                        <a href="#">View more</a>
+                    </figcaption>
+                </figure>
+                <figure class="effect-julia item">
+                    <img src="img/3.jpeg" alt="Image">
+                    <figcaption>
+                        
+                        <a href="#">View more</a>
+                    </figcaption>
+                </figure>
+                <figure class="effect-julia item">
+                    <img src="img/4.jpeg" alt="Image">
+                    <figcaption>
+                        
+                        <a href="#">View more</a>
+                    </figcaption>
+                </figure>
+                <figure class="effect-julia item">
+                    <img src="img/5.jpeg" alt="Image">
+                    <figcaption>
+                        
+                        <a href="#">View more</a>
+                    </figcaption>
+                </figure>
+                <figure class="effect-julia item">
+                    <img src="img/6.jpeg" alt="Image">
+                    <figcaption>
+                        
+                        <a href="#">View more</a>
+                    </figcaption>
+                </figure>
+                
+              </div>
+            </div>
+          </li>
+          <li data-page-no="3" class="px-3">
+            <div class="position-relative page-width-1 page-right tm-border-top tm-border-bottom">
+              <div class="circle intro-circle-1"></div>
+              <div class="circle intro-circle-2"></div>
+              <div class="circle intro-circle-3"></div>
+              <div class="circle intro-circle-4"></div>
+              <div class="tm-bg-dark content-pad">
+                <h2 class="mb-4">Tentang Saya</h2>
+                <p class="mb-4">
+                  Perkenalkan nama saya mazmur ronaldo panjaitan sering di panggil ronald, keseharian saya hanya melaksanakan kuliah dan mengerjakan tugas-tugas yg di berikan. Saya dari kota Pekanbaru provinsi Riau, tempat kampus saya di Stmik Amik Riau, jurusan teknik informatika.
+                </p>
+                <p>
+                  Saya sedang melakukan program studi independen di NF Computer pelatihan sebagai Fullstack Web Developer.
+                </p>
+              </div>              
+            </div>
+          </li>
+          <li data-page-no="4">
+            <div class="mx-auto page-width-2">
+              <div class="row">
+                <div class="col-md-6 me-0 ms-auto">
+                  <h2 class="mb-4">Contact </h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 tm-contact-left">
+                  <form action="#" method="POST" class="contact-form">
+                    <div class="input-group tm-mb-30">
+                        <input name="name" type="text" class="form-control rounded-0 border-top-0 border-end-0 border-start-0" placeholder="Name">
+                    </div>
+                    <div class="input-group tm-mb-30">
+                        <input name="email" type="text" class="form-control rounded-0 border-top-0 border-end-0 border-start-0" placeholder="Email">
+                    </div>
+                    <div class="input-group tm-mb-30">
+                        <textarea rows="5" name="message" class="textarea form-control rounded-0 border-top-0 border-end-0 border-start-0" placeholder="Message"></textarea>
+                    </div>
+                    <div class="input-group justify-content-end">
+                        <input type="submit" class="btn btn-primary tm-btn-pad-2" value="Send">
+                    </div>
+                  </form>
+                </div>
+                <div class="col-md-6 tm-contact-right">                  
+                  <p class="mb-4">
+                    Hubungi Saya jika ada hal yang perlu di tanyakan tentang saya
+                  </p>
+                  <div>
+                    Email: <a href="mailto:ronaldo.panjaitan7@gmail.com" class="tm-link-white">ronaldo.panjaitan7@gmail.com</a>
+                  </div>
+                  <div class="tm-mb-45">
+                    Tel: <a href="tel:085741104367" class="tm-link-white">085741104367</a>
+                  </div>
+                  <!-- Map -->
+                  <div class="map-outer">
+                    <div class="gmap-canvas">
+                        <iframe width="100%" height="400" id="gmap-canvas"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127669.05293968163!2d101.3711347871528!3d0.5137908161201079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5ab80690ee7b1%3A0x94dde92c3823dbe4!2sPekanbaru%2C%20Kota%20Pekanbaru%2C%20Riau!5e0!3m2!1sid!2sid!4v1647172486146!5m2!1sid!2sid"
+                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    </div>
+                </div>
+                </div>
+              </div>
+            </div>            
+          </li>
+        </ul>
+    </div>
+    <div class="container-fluid">
+      <footer class="row mx-auto tm-footer">
+        
+        <div class="col-md-6 px-0 tm-footer-right">
+          Designed by <a rel="sponsored" href="https://templatemo.com" target="_blank" class="tm-link-white"></a>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- Preloader, https://ihatetomatoes.net/create-custom-preloading-screen/ -->
+  <div id="loader-wrapper">            
+    <div id="loader"></div>
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
+  </div>  
+  <script src="js/jquery-3.5.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/slick.js"></script>
+  <script src="js/templatemo-script.js"></script>
+</body>
+</html>
